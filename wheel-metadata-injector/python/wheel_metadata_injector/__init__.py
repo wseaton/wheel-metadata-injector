cac2a2dda29981554275ff360e58ee356ce7cab3
Will Eaton <me@wseaton.com>
2025-05-06 12:08:08 -0400

    fix setuptools missing import affecting cli

--- wheel-metadata-injector/python/wheel_metadata_injector/__init__.py
+++ wheel-metadata-injector/python/wheel_metadata_injector/__init__.py
@@ -6,7 +6,6 @@ from ._wheel_metadata_injector import (
     get_whitelisted_env_vars_with_file,
     get_env_vars_from_comma_list,
 )
-from .setuptools_plugin import InjectMetadataBdistWheel
 
 __all__ = [
     "process_wheel",
@@ -15,5 +14,13 @@ __all__ = [
     "get_whitelisted_env_vars",
     "get_whitelisted_env_vars_with_file",
     "get_env_vars_from_comma_list",
-    "InjectMetadataBdistWheel",
 ]
+
+try:
+    from .setuptools_plugin import InjectMetadataBdistWheel  # noqa: F401
+
+    __all__.append("InjectMetadataBdistWheel")
+except ImportError:
+    # If setuptools is not available, we can still import the other functions (like what the CLI requires),
+    # but we cannot define the InjectMetadataBdistWheel class.
+    pass
